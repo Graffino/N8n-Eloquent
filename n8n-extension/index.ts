3555e9b01438963dc96f548cc866e9df381fed61
Nick Ciolpan <nick@graffino.com>
2025-06-02 15:15:07 +0300

    Clean up n8n extension: remove old LaravelEloquentSet node and fix loading issues

--- n8n-extension/index.ts
+++ n8n-extension/index.ts
@@ -1,11 +1,9 @@
 import { LaravelEloquentApi } from './credentials/LaravelEloquentApi.credentials';
 import { LaravelEloquentTrigger } from './nodes/LaravelEloquentTrigger/LaravelEloquentTrigger.node';
 import { LaravelEloquentGet } from './nodes/LaravelEloquentGet/LaravelEloquentGet.node';
-import { LaravelEloquentSet } from './nodes/LaravelEloquentSet/LaravelEloquentSet.node';
 
 export {
 	LaravelEloquentApi,
 	LaravelEloquentTrigger,
 	LaravelEloquentGet,
-	LaravelEloquentSet,
 }; 
